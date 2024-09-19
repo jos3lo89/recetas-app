99b715190de0de85009a0af16df4766e42293c00
jos3lo89 <gothangel21@hotmail.com>
2024-09-19 18:32:25 -0500

    finished views

--- src/app/home/components/card-recommendations/card-recommendations.component.ts
+++ src/app/home/components/card-recommendations/card-recommendations.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  IonList,
+  IonGrid,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonCard,
+  IonLabel,
+  IonListHeader,
+} from '@ionic/angular/standalone';
+import { categorias } from '../card-categorie/categories';
 
 @Component({
   selector: 'app-card-recommendations',
   templateUrl: './card-recommendations.component.html',
   styleUrls: ['./card-recommendations.component.scss'],
   standalone: true,
+  imports: [
+    IonListHeader,
+    IonLabel,
+    IonCard,
+    IonItem,
+    IonCardContent,
+    IonCardTitle,
+    IonCardHeader,
+    IonGrid,
+    IonList,
+  ],
 })
 export class CardRecommendationsComponent implements OnInit {
-  constructor() {}
+  categorias = categorias;
 
+  constructor() {}
   ngOnInit() {}
 }
